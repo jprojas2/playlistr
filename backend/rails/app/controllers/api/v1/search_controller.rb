04ff6bcdf687bb6cd0d9256ef071c53b7b29bd50
Juan Pablo Rojas <jprojasc2@gmail.com>
2024-04-17 18:34:19 -0400

    sort results by relevance

--- backend/rails/app/controllers/api/v1/search_controller.rb
+++ backend/rails/app/controllers/api/v1/search_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::SearchController < Api::V1::ApiController
 
   # GET /search
   def index
-    render json: Genius.new.search(params[:q])
+    render json: process_search(Genius.new.search(params[:q]))
+  end
+
+  def process_search search_results
+    artists = search_results.map{|e| e.dig("primary_artist", "name") =~ /#{params[:q]}/i && e["primary_artist"]}.compact.flatten.uniq
+    hot_songs = search_results.select{|e| (e.dig("stats", "pageviews") || 0) > 500_000 && e["title"] =~ /#{params[:q]}/i}
+    (hot_songs + artists + (search_results - hot_songs)).uniq
   end
 end
