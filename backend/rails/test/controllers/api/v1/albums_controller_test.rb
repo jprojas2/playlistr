f5bcccb44adf900b2622541f4bb93e3d1b2469f4
Juan Pablo Rojas <jprojasc2@gmail.com>
2024-04-24 22:00:28 -0400

    fix album controller test

--- backend/rails/test/controllers/api/v1/albums_controller_test.rb
+++ backend/rails/test/controllers/api/v1/albums_controller_test.rb
@@ -10,10 +10,7 @@ class AlbumsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should not be able to access auth protected actions without a authorization' do
     assert_auth_endpoint(:get, api_v1_albums_url)
-    assert_auth_endpoint(:post, api_v1_albums_url)
     assert_auth_endpoint(:get, api_v1_album_url(@album))
-    assert_auth_endpoint(:patch, api_v1_album_url(@album))
-    assert_auth_endpoint(:delete, api_v1_album_url(@album))
   end
 
   test "should get index" do
@@ -21,29 +18,8 @@ class AlbumsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create album" do
-    assert_difference("Album.count") do
-      post api_v1_albums_url, params: { album: { eid: "1234", image_url: @album.image_url, name: @album.name, year: @album.year } }, as: :json, headers: @headers
-    end
-
-    assert_response :created
-  end
-
   test "should show album" do
     get api_v1_album_url(@album), as: :json, headers: @headers
     assert_response :success
-  end
-
-  test "should update album" do
-    patch api_v1_album_url(@album), params: { album: { eid: @album.eid, image_url: @album.image_url, name: @album.name, year: @album.year } }, as: :json, headers: @headers
-    assert_response :success
-  end
-
-  test "should destroy album" do
-    assert_difference("Album.count", -1) do
-      delete api_v1_album_url(@album), as: :json, headers: @headers
-    end
-
-    assert_response :no_content
   end
 end
