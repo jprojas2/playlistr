ad16ecaa6d4688bf63305c90e2db9d56956664db
Juan Pablo Rojas <jprojasc2@gmail.com>
2024-07-20 19:23:39 -0400

    fix user not loading after login

--- frontend/react-ts/src/contexts/AuthContext.tsx
+++ frontend/react-ts/src/contexts/AuthContext.tsx
@@ -32,7 +32,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
                 setUser(res.data)
             })
         }
-    }, [])
+    }, [token])
 
     const contextValue = useMemo(
         () => ({
